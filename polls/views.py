d2329563764b9518a2dfc4f13be8bf7ae5a02513
Arisa P <chilizhiki@gmail.com>
2019-11-04 14:51:19 +0700

    test

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,12 @@
+import logging
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-
 from .models import Choice, Question
 
-
+logger = logging.getLogger()
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
